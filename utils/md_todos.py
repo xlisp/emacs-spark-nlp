0daaa3d6709bd9861429ccd3b4568bad1ba9edf1
stevechan <chanshunli@gmail.com>
2024-10-19 09:09:40 +0800

    Add get_pattern_items

--- utils/md_todos.py
+++ utils/md_todos.py
@@ -13,3 +13,16 @@ def get_todo_items(directory):
                     todo_items.append((filename, todo.strip()))
     return todo_items
 
+def get_pattern_items(directory, pattern):
+    """ python script.py /path/to/directory "^.*(?:todo|TODO|fixme|FIXME).*$" """
+    pattern_items = []
+    for filename in os.listdir(directory):
+        if filename.endswith('.md'):
+            filepath = os.path.join(directory, filename)
+            with open(filepath, 'r', encoding='utf-8') as file:
+                content = file.read()
+                todos = re.findall(pattern, content, re.MULTILINE)
+                for todo in todos:
+                    pattern_items.append((filename, todo.strip()))
+    return pattern_items
+
